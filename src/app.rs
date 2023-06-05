d88dba00c6c2a137145f95795ff763e098fa9cc4
Alexey Golubev <dr.freecx@gmail.com>
2023-06-05 11:47:36 +0300

    feat: simple keep-alive connection with multithreading

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 use crate::http;
 use crate::status;
@@ -8,12 +9,14 @@ use status::StatusCode;
 
 type RouteFunc = fn(HttpData) -> HttpData;
 
+#[derive(Clone)]
 struct Route {
     url: String,
     method: HttpMethod,
     func: RouteFunc,
 }
 
+#[derive(Clone)]
 pub struct App {
     host: String,
     port: u16,
@@ -51,30 +54,47 @@ impl App {
     }
 
     fn handle_client(&self, mut stream: TcpStream) -> Option<()> {
-        let mut request = HttpData::new();
-        request.addr = stream.peer_addr().ok();
-        request.parse(&mut stream)?;
-
-        if let Some(addr) = request.addr {
+        if let Ok(addr) = stream.peer_addr() {
             println!(">>> incoming connection from {}:{}", addr.ip(), addr.port());
         }
 
-        println!(">>> {:?} {}\n{}", request.method, request.url, request.render_headers());
+        loop {
+            let mut request = HttpData::new();
+            request.addr = stream.peer_addr().ok();
+            request.parse(&mut stream)?;
 
-        let mut response = self
-            .route(&request.url, request.method)
-            .map(|r| (r.func)(request))
-            .unwrap_or(HttpData::from_status(StatusCode::NotFound));
+            let keep_alive = match request.headers.get("connection") {
+                Some(text) => text == "keep-alive",
+                _ => false,
+            };
 
-        // add server info
-        response.add_header("host", format!("{}:{}", self.host, self.port));
-        response.add_header("server", "micro-http/0.1");
+            println!(">>> {:?} {}\n{}", request.method, request.url, request.render_headers());
 
-        println!("<<< HTTP/1.1 {}\n{}", response.status_code, response.render_headers());
-        write!(stream, "HTTP/1.1 {}\r\n{}\r\n", response.status_code, response.render_headers()).ok()?;
-        if let Some(content) = response.content {
-            stream.write_all(content.as_slice()).ok()?;
+            let mut response = self
+                .route(&request.url, request.method)
+                .map(|r| (r.func)(request))
+                .unwrap_or(HttpData::from_status(StatusCode::NotFound));
+
+            // add server info
+            response.add_header("host", format!("{}:{}", self.host, self.port));
+            response.add_header("server", "micro-http/0.1");
+            if keep_alive {
+                response.add_header("connection", "keep-alive");
+            }
+
+            println!("<<< HTTP/1.1 {}\n{}", response.status_code, response.render_headers());
+            write!(stream, "HTTP/1.1 {}\r\n{}\r\n", response.status_code, response.render_headers()).ok()?;
+            if let Some(content) = response.content {
+                stream.write_all(content.as_slice()).ok()?;
+            }
+
+            // TODO: таймаут для keep-alive ?
+            if !keep_alive {
+                break;
+            }
         }
+
+        println!("--- end of connection ---");
 
         Some(())
     }
@@ -85,8 +105,12 @@ impl App {
         let listener = TcpListener::bind(&addr).ok()?;
         println!(">>> run server @ {addr}");
 
+        // TODO: использовать ограниченное число потоков
         for stream in listener.incoming().flatten() {
-            self.handle_client(stream)?;
+            let app_clone = self.clone();
+            thread::spawn(move || {
+                let _ = app_clone.handle_client(stream);
+            });
         }
 
         Some(())
